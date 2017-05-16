2ef50d99289a943d4dd28f3299eb9d3707aea5b0
Andy Ennamorato <andy@artifactuprising.com>
2017-05-15 22:27:54 -0600

    Slightly fancier buttons

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import RaisedButtonExampleComponent from './RaisedButtonExampleComponent';
+import darkBaseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
 
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-        <MuiThemeProvider>
-	    <MyAwesomeReactComponent />
-	</MuiThemeProvider>
+        <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
+	        <body>
+            <AppBar title="I want it now!" />
+            <RaisedButtonExampleComponent />
+          </body>
+        </MuiThemeProvider>
     );
   }
 }
